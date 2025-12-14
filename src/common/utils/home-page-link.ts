ae85ccdf15419c78f4b2dab3caeecefb64fa4942
suleigolden <suleimamman@gmail.com>
2025-12-14 11:31:06 -0500

    fix: fix render deploy issue

--- src/common/utils/home-page-link.ts
+++ src/common/utils/home-page-link.ts
@@ -3,9 +3,9 @@ import { User } from "@suleigolden/the-last-spelling-bee-api-client";
 
 export const homePageLink = (user: User) => {
     if(user.role === "system-admin") {
-        return `/user/${user.id}/dashboard`;
+        return `/admin/${user.id}/dashboard`;
     }else if(user.role === "character") {
-        return `/character/${user.id}/dashboard`;
+        return `/user/${user.id}/dashboard`;
     }else{
         return `/`;
     }
